6aff7f8d3c9cae933efa8359b7bbab2cc20508a8
gh_feng <gg583399013@qq.com>
2016-12-17 20:02:34 +0800

    io字节流操作

---
+++ java-base/src/main/java/io/test001/Student.java
@@ -0,0 +1,20 @@
+package io.test001;
+
+import java.io.Serializable;
+
+class Student implements Serializable {
+	private String name;
+	private int age;
+
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + "]";
+	}
+
+}
